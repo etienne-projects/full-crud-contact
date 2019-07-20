03612cde469bcd16a640a5cc6973981163f41c48
Etienne <phone.mfe@gmail.com>
2019-07-20 14:06:28 -0400

    update code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
-let JSONContacts = JSON.parse("data/Contacts.json");
+// let JSONContacts = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
+let JSONContacts = JSON.parse('../data/contacts.txt');
 let contactListElement = document.getElementById('contact-list');
 let nameInputElement = document.getElementById('contact_name');
 let emailInputElement = document.getElementById('contact_email');
@@ -7,14 +8,5 @@ let emailInputElement = document.getElementById('contact_email');
 const addContactButton = document.getElementById('btn-add-contact');
 addContactButton.addEventListener('click', addContact);
 
-populateContactsArea();
+// populateContactsArea();
 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = this.responseText;
-    document.getElementById("demo").innerHTML = myObj.name;
-  }
-};
-request.open("GET", "../data/Contacts.json", true);
-request.send();
